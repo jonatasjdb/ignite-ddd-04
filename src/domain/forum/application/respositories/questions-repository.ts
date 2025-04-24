a057db163416964e5821b0bcea556298b7174b9d
Jônatas Dias <jonatasjdb@hotmail.com>
2025-04-23 22:55:09 -0300

    Adicionando edições de perguntas e respostas

--- src/domain/forum/application/respositories/questions-repository.ts
+++ src/domain/forum/application/respositories/questions-repository.ts
@@ -3,6 +3,7 @@ import { Question } from '../../enterprise/entities/question'
 export interface QuestionsRepository {
   findById(id: string): Promise<Question | null>
   findBySlug(slug: string): Promise<Question | null>
-  create(answer: Question): Promise<void>
+  create(question: Question): Promise<void>
+  save(question: Question): Promise<void>
   delete(question: Question): Promise<void>
 }
---
+++ src/domain/forum/application/use-cases/edit-question.ts
@@ -0,0 +1,34 @@
+import { QuestionsRepository } from '../respositories/questions-repository'
+
+interface EditQuestionUseCaseRequest {
+  questionId: string
+  authorId: string
+  title: string
+  content: string
+}
+
+export class EditQuestionUseCase {
+  constructor(private questionsRepository: QuestionsRepository) {}
+
+  async execute({
+    authorId,
+    questionId,
+    title,
+    content,
+  }: EditQuestionUseCaseRequest) {
+    const question = await this.questionsRepository.findById(questionId)
+
+    if (!question) {
+      throw new Error('Question not found')
+    }
+
+    if (authorId !== question.authorId.toString()) {
+      throw new Error('Not allowed.')
+    }
+
+    question.title = title
+    question.content = content
+
+    await this.questionsRepository.save(question)
+  }
+}
